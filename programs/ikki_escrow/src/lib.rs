4b9fbe3d1f35925152ad6828bef0d5a6820a6bfa
shivaraj110 <shivarajchandaragi9@gmail.com>
2026-03-05 12:24:48 +0530

    added real keypairs and pubkey

--- programs/ikki_escrow/src/lib.rs
+++ programs/ikki_escrow/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("7rP4rHKBqnYGB2UgpeRtd9f3FZ1PHwfc4iPVhWRv9UP4");
 
 // ─── Constants ──────────────────────────────────────────────────────────────────
 
